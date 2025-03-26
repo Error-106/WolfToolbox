f978d47bbf1c295ba18955050775c46c7ab3bf95
xionglongztz <1325452715@qq.com>
2025-03-26 15:01:13 +0800

    新增多种功能：玩家权限执行指令，修复飞行权限，修复部分文本显示错误等

--- src/main/java/com/xionglongztz/wolfevents.java
+++ src/main/java/com/xionglongztz/wolfevents.java
@@ -1,9 +1,10 @@
 package com.xionglongztz;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;//玩家
 import org.bukkit.event.EventHandler;//用于注册事件监视器
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;//玩家加入游戏事件
-import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.*;
 
 import java.net.http.HttpRequest;//HTTP请求
 import java.net.http.HttpClient;
@@ -16,32 +17,42 @@ import static org.bukkit.Bukkit.getLogger;
 public class wolfevents implements Listener {
     private static final String PREFIX = "§f[ §b小狼工具箱 §f] ";// 插件前缀
     private static final String FLY_PERMISSION = "wolftoolbox.fly";// 飞行权限
-    private static final String MSG_PERMISSION = "wolftoolbox.msg";// 管理员权限
+    private static final String MSG_PERMISSION = "wolftoolbox.msg";// 一言权限
+
     @EventHandler
-    public void onWorldChange(PlayerChangedWorldEvent e) {
+    public void onWorldChange(PlayerChangedWorldEvent e) {// 玩家所在世界变化
         Player player = e.getPlayer();
-        if (!player.hasPermission(FLY_PERMISSION)) {
-            player.setFlying(false);
-            player.setAllowFlight(false);
+        if (player.hasPermission(FLY_PERMISSION)) {
+            player.setAllowFlight(true);// 允许玩家飞行
         } else {
-            player.setAllowFlight(true);
-            player.setFlying(true);
+            player.setFlying(false);// 关闭飞行
+            player.setAllowFlight(false);// 不允许玩家开启飞行
+            player.sendMessage(PREFIX + "§c当前世界无飞行权限，已关闭飞行功能！");// 告诉玩家已被关闭
         }
     }// 当改变玩家所在世界时，若没有权限，则关闭
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
+    public void onPlayerJoin(PlayerJoinEvent event) {//玩家进入游戏
         Player player = event.getPlayer();
         if (player.hasPermission(MSG_PERMISSION)) {//检查玩家是否拥有权限
             try {
                 HttpRequest request = HttpRequest.newBuilder()
                         .uri(URI.create("https://v1.hitokoto.cn/?encode=text"))//从 Web 服务器获取纯文本数据
                         .build();
-                HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+                HttpResponse<String> response = HttpClient.newHttpClient()
+                        .send(request, HttpResponse.BodyHandlers.ofString());
                 String message = response.body();
-                player.sendMessage(PREFIX + "§e一言：§b" + message);//发送消息
+                player.sendMessage("§e一言:§b" + message);//发送消息
             } catch (IOException | InterruptedException e) {
-                getLogger().severe("无法获取一言：" + e.getMessage());
+                getLogger().severe("无法获取一言:" + e.getMessage());
             }
         }
+    }
+    @EventHandler(priority = EventPriority.MONITOR)// 最高优先级
+    public void onPlayerCommand(PlayerCommandPreprocessEvent e) {// 监视指令发送，输出到控制台
+        Player player = e.getPlayer(); // 获得玩家信息
+        String command = e.getMessage(); // 获得指令内容
+        String customCmdMsg = "§9§o*" + player.getName() + "§r§f: §7" + command;// 构建消息
+        Bukkit.getConsoleSender().sendMessage(customCmdMsg);// 向控制台发送构建后的消息
+        // WT无法干预控制台记录命令到日志，所以需要将Spigot的commands/log设为False才可以避免重复记录！
     }
 }
--- src/main/java/com/xionglongztz/wolftoolbox.java
+++ src/main/java/com/xionglongztz/wolftoolbox.java
@@ -1,13 +1,16 @@
 package com.xionglongztz;// 当前Java类包名
 import org.bukkit.Bukkit;// Bukkit API
-import org.bukkit.GameMode;
+import org.bukkit.GameMode;// 获取玩家游戏模式
 import org.bukkit.command.ConsoleCommandSender;// 表示服务器控制台
 import org.bukkit.plugin.java.JavaPlugin;// Bukkit插件的主类
 import org.bukkit.command.Command;// 表示服务器命令
 import org.bukkit.command.CommandSender;// 发送命令的实体
 import org.bukkit.entity.Player;// 访问玩家信息和操作玩家的方法
 import org.jetbrains.annotations.NotNull;// 标记参数或返回值不能为空
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public final class wolftoolbox extends JavaPlugin{
     private static final String PREFIX = "§f[ §b小狼工具箱 §f] ";// 插件前缀
@@ -27,17 +30,37 @@ public final class wolftoolbox extends JavaPlugin{
 
     @Override
     public void onEnable() {
+        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();// 获得控制台消息sender
         saveDefaultConfig();
         reloadConfig();
-        getServer().getPluginManager().registerEvents(new wolfevents(), this);
-        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-        console.sendMessage("§f[ §b小狼工具箱 §f] §a插件已启用！");
+        getServer().getPluginManager().registerEvents(new wolfevents(), this);// 注册事件监听器
+        console.sendMessage(PREFIX + "§a插件已启用！");
     }
     @Override
     public void onDisable() {
         ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-        console.sendMessage( "§f[ §b小狼工具箱 §f] §c插件已禁用！");
+        console.sendMessage(PREFIX + "§c插件已禁用！");
     }
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, Command command, @NotNull String alias, String[] args) {
+        if (command.getName().equalsIgnoreCase("gm")) {
+            if (args.length == 1) {// 返回预设的候选词列表
+                List<String> suggestions = new ArrayList<>();
+                suggestions.add("c");
+                suggestions.add("s");
+                suggestions.add("a");
+                suggestions.add("p");
+                return suggestions;
+            }
+        } else if (command.getName().equalsIgnoreCase("sudo")) {
+            if (args.length == 2) {// 返回预设的候选词列表
+                List<String> suggestions = new ArrayList<>();
+                suggestions.add("");
+                return suggestions;// 这里是遍历服务器可用指令的，但是我不会写
+            }
+        }
+        return null; // 返回 null 表示使用默认的 Tab 补全
+    }// 修改候选词
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         switch (command.getName().toLowerCase()) {// 判断玩家输入的指令
             case "enderchest" -> {
@@ -72,7 +95,7 @@ public final class wolftoolbox extends JavaPlugin{
                 handleGrindstoneCommand(sender, args);
                 return true;
             }
-            case "wtbhelp" -> {
+            case "wthelp" -> {
                 handleHelpCommand(sender);
                 return true;
             }
@@ -82,6 +105,14 @@ public final class wolftoolbox extends JavaPlugin{
             }
             case "fly" -> {
                 handleFlyCommand(sender, args);
+                return true;
+            }
+            case "gm" -> {
+                handleGamemodeCommand(sender, args);
+                return true;
+            }
+                case "sudo" -> {
+                handleSudoCommand(sender, args);
                 return true;
             }
             default -> {
@@ -333,7 +364,7 @@ public final class wolftoolbox extends JavaPlugin{
             Player target = Bukkit.getPlayer(args[0]);
             if (target != null) {//参数不为空
                 if (target.getGameMode() == GameMode.CREATIVE || target.getGameMode() == GameMode.SPECTATOR){//目标是否旁观创造
-                    sender.sendMessage(PREFIX + "§c无法在创造/旁观模式切换§e " + sender.getName() + " §c的飞行状态！");// 向发送者报错
+                    sender.sendMessage(PREFIX + "§c无法在创造/旁观模式切换§e " + target.getName() + " §c的飞行状态！");// 向发送者报错
                     return;
                 }
                 if (target.isFlying()){
@@ -380,7 +411,10 @@ public final class wolftoolbox extends JavaPlugin{
             sender.sendMessage("§b/grindstone §a[Player] §e打开砂轮");
             sender.sendMessage("§b/suicide §e自杀");
             sender.sendMessage("§b/fly §a[Player] §e开启或关闭飞行");
-            sender.sendMessage("§b/wtbhelp §e显示本帮助");
+            sender.sendMessage("§b/wthelp §e显示本帮助");
+            sender.sendMessage("§b/gm §e<c(1)/s(2)/a(3)/p(4)> §a[Player] ");
+            sender.sendMessage("    §e切换游戏模式为§ac/1(创造)§es/2(生存)§ca/3(冒险)§7p/4(旁观)");
+            sender.sendMessage("§b/sudo §e<Player> §e[<Command>|<Text>] §e强制指定玩家发送指令/消息");
             sender.sendMessage("§b======================================");
         } else {
             sender.sendMessage("§b==========§a小狼工具箱§b==========");
@@ -394,8 +428,114 @@ public final class wolftoolbox extends JavaPlugin{
             sender.sendMessage("§b/grindstone §e打开砂轮");
             sender.sendMessage("§b/suicide §e自杀");
             sender.sendMessage("§b/fly §e开启或关闭飞行");
-            sender.sendMessage("§b/wtbhelp §e显示本帮助");
+            sender.sendMessage("§b/wthelp §e显示本帮助");
             sender.sendMessage("§b=============================");
         }
     }// 显示帮助信息
+    private void handleSudoCommand(CommandSender sender, String[] args) {
+        if (sender.hasPermission(ADMIN_PERMISSION)) {// 是否具有权限
+            if (args.length == 0) {// 判断是否存在参数
+                sender.sendMessage(PREFIX + "§c语法错误，该指令不能单独执行！");
+            } else if (args.length == 1) {// 判断参数为1时的情况，只为当前输入玩家修改模式
+                Player target = Bukkit.getPlayer(args[0]);// 获取被操作的玩家id
+                if (target != null) {
+                    sender.sendMessage(PREFIX + "§c语法错误：请指定玩家 §e" + target.getName() + " §c要输入的指令/消息！");
+                } else {
+                    sender.sendMessage(PREFIX + "§c玩家 §e" + args[0] + " §c不在线！");
+                }
+            } else {
+                Player target = Bukkit.getPlayer(args[0]);// 获取被操作的玩家id
+                StringBuilder commandToExecute = new StringBuilder();
+                for (int i = 1; i < args.length; i++) {
+                    commandToExecute.append(args[i]).append(" ");
+                }// 获得想要输入的命令
+                if (target != null) { // 当玩家id不为空时
+                    if (target.hasPermission(commandToExecute.substring(1))) {
+                        target.performCommand(commandToExecute.toString());
+                        sender.sendMessage(PREFIX + "§a已让玩家 §e" + target.getName() + " §a执行指令 §b" + commandToExecute);
+                    } else {
+                        sender.sendMessage(PREFIX + "§c玩家 §e" + target.getName() + " §c没有权限执行 §b" + commandToExecute);
+                    }
+                } else {
+                    sender.sendMessage(PREFIX + "§c玩家 §e" + args[0] + " §c不在线！");// 玩家离线
+                }
+            }
+        } else {
+        sender.sendMessage(NO_PERMISSION);// 提示无权限
+        }
+    }// 让指定玩家说话/执行指令
+    private void handleGamemodeCommand(CommandSender sender, String[] args) {
+        if (sender.hasPermission(ADMIN_PERMISSION)) {// 是否具有权限
+            if (args.length == 0) {// 判断是否存在参数
+                sender.sendMessage(PREFIX + "§c请输入确切的游戏模式！参数只能为§ac/1(创造)§es/2(生存)§ca/3(冒险)§7p/4(旁观)");
+            } else if (args.length == 1) {// 判断参数为1时的情况，只为当前输入玩家修改模式
+                if (sender instanceof Player player) {// 是否为玩家执行
+                    switch (args[0]) {
+                        case "c":
+                        case "1":
+                            player.sendMessage(PREFIX + "§a已切换为§a§o创造模式");
+                            player.setGameMode(GameMode.CREATIVE);
+                            break;
+                        case "s":
+                        case "2":
+                            player.sendMessage(PREFIX + "§a已切换为§e§o生存模式");
+                            player.setGameMode(GameMode.SURVIVAL);
+                            break;
+                        case "a":
+                        case "3":
+                            player.sendMessage(PREFIX + "§a已切换为§c§o冒险模式");
+                            player.setGameMode(GameMode.ADVENTURE);
+                            break;
+                        case "p":
+                        case "4":
+                            player.sendMessage(PREFIX + "§a已切换为§7§o旁观模式");
+                            player.setGameMode(GameMode.SPECTATOR);
+                            break;
+                        default:
+                            player.sendMessage(PREFIX + "§c游戏模式无效！参数只能为§ac/1(创造)§es/2(生存)§ca/3(冒险)§7p/4(旁观)");
+                    }
+                } else {
+                    sender.sendMessage(ONLY_PLAYER);// 提示只能由玩家执行
+                }
+            } else if (args.length == 2) {
+                Player target = Bukkit.getPlayer(args[1]);// 获取被操作的玩家id
+                if (target != null) { // 当玩家id不为空时
+                    switch (args[0]) {
+                        case "c":
+                        case "1":
+                            sender.sendMessage(PREFIX + "§a已切换玩家 §e" + target.getName() + " §a的游戏模式为§a§o创造模式");
+                            target.sendMessage(PREFIX + "§a管理员 §e" + sender.getName() + " §a已切换你的游戏模式为§a§o创造模式");
+                            target.setGameMode(GameMode.CREATIVE);
+                            break;
+                        case "s":
+                        case "2":
+                            sender.sendMessage(PREFIX + "§a已切换玩家 §e" + target.getName() + " §a的游戏模式为§e§o生存模式");
+                            target.sendMessage(PREFIX + "§a管理员 §e" + sender.getName() + " §a已切换你的游戏模式为§e§o生存模式");
+                            target.setGameMode(GameMode.SURVIVAL);
+                            break;
+                        case "a":
+                        case "3":
+                            sender.sendMessage(PREFIX + "§a已切换玩家 §e" + target.getName() + " §a的游戏模式为§c§o冒险模式");
+                            target.sendMessage(PREFIX + "§a管理员 §e" + sender.getName() + " §a已切换你的游戏模式为§c§o冒险模式");
+                            target.setGameMode(GameMode.ADVENTURE);
+                            break;
+                        case "p":
+                        case "4":
+                            sender.sendMessage(PREFIX + "§a已切换玩家 §e" + target.getName() + " §a的游戏模式为§7§o旁观模式");
+                            target.sendMessage(PREFIX + "§a管理员 §e" + sender.getName() + " §a已切换你的游戏模式为§7§o旁观模式");
+                            target.setGameMode(GameMode.SPECTATOR);
+                            break;
+                        default:
+                            sender.sendMessage(PREFIX + "§c游戏模式无效！参数只能为§ac/1(创造)§es/2(生存)§ca/3(冒险)§7p/4(旁观)");
+                    }
+                } else {
+                    sender.sendMessage(PREFIX + "§c玩家 §e" + args[1] + " §c不在线！");// 玩家离线
+                }
+            } else {
+                sender.sendMessage(PREFIX + "§c未知参数！请使用 /wthelp 查看帮助");// 参数错误
+            }
+        } else {
+            sender.sendMessage(NO_PERMISSION);// 提示无权限
+        }
+    }// 切换游戏模式
 }
